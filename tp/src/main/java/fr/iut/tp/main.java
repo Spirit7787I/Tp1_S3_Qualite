f65726f6c4f0ee5d1cd210c38a5e19a73ab67432
Spirit7781I <aandrademobile@gmail.com>
2026-09-09 15:32:12 +0200

    Ajout dans le main affichage

--- tp/src/main/java/fr/iut/tp/main.java
+++ tp/src/main/java/fr/iut/tp/main.java
@@ -4,5 +4,6 @@ package fr.iut.tp;
 public class App {
     public static void main(String[] args) {
         System.out.println("Calculatrice");
+	System.out.println("Addition fonctionelle");
     }
 }
