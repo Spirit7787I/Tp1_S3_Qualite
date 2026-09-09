43963420e8a3dce227d416b3c0d02a0fd19cf9ff
Spirit7781I <aandrademobile@gmail.com>
2026-09-09 15:27:28 +0200

    Main calculatrice

--- tp/src/main/java/fr/iut/tp/main.java
+++ tp/src/main/java/fr/iut/tp/main.java
@@ -1,10 +1,8 @@
 package fr.iut.tp;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Calculatrice");
     }
 }
